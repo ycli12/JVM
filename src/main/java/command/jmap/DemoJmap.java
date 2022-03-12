e131902f9ac7a4fe7d95fc36e9245aa0f5991c5f
ycli12 <ucos-iii@qq.com>
2022-03-12 08:31:18 +0800

    [feat] 新增jstat命令使用记录

---
+++ src/main/java/command/jmap/DemoJmap.java
@@ -0,0 +1,8 @@
+package command.jmap;
+
+public class DemoJmap {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("jmap 命令...");
+        Thread.sleep(100 * 1000L);
+    }
+}
